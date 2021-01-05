3f8775e0650ed51e3e66df6d5aa37b3e3f927e12
Adam Tomys <adamtomys@gmail.com>
2021-01-05 15:10:38 +0100

    first changes

--- src/components/views/Countries/CountriesContainer.js
+++ src/components/views/Countries/CountriesContainer.js
@@ -2,8 +2,13 @@ import {connect} from 'react-redux';
 import Countries from './Countries';
 import {getAllCountries} from '../../../redux/countriesRedux';
 
-const mapStateToProps = state => ({
-  countries: getAllCountries(state),
-});
+const mapStateToProps = (state, props) => {
+  const id = props.match.params.id;
+  
+  return {
+    id: id,
+    countries: getAllCountries(state),
+  };
+};
 
 export default connect(mapStateToProps)(Countries);
